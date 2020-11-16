ac76f4b63b1f257b82535700f4e1038e19c6cebd
superior-prog <meherajmahmmd@gmail.com>
2020-11-16 14:14:42 +0600

    Part 3 Completed

---
+++ ecommerce/store/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.store, name="store"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('update-item/', views.updateItem, name="update-item"),
+    path('cart/update-item/', views.updateItem, name="update-item"),
+    path('checkout/process-order/', views.processOrder, name="process-order"),
+]
