9fbde173643e3848387a8944bf7560a9d727ae5b
superior-prog <meherajmahmmd@gmail.com>
2021-01-11 20:13:47 +0600

    Update models.py

--- ecommerce/user/models.py
+++ ecommerce/user/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(null=True, blank=True)
+    phone = models.CharField(max_length=15, null=True, blank=True)
 
     def __str__(self):
-        return self.user.email
+        return str(self.user.id)
